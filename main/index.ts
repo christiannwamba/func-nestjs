bda6e5a0e142e4108c8b21494b6c23f19801c6c4
Christian Nwamba <nwambachristian@gmail.com>
2019-06-18 13:19:27 +0400

    no response

--- main/index.ts
+++ main/index.ts
@@ -1,8 +1,11 @@
 // import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-const createHandler = require("azure-function-express").createHandler;
-import { createApp } from '../src/main'
+const createHandler = require('azure-function-express').createHandler;
+import { createApp } from '../src/main';
+
+
 
 export default async function() {
-    const app = await createApp()
-    return createHandler(app)
-}
+    const app = await createApp();
+    const instance = app.getHttpAdapter().getInstance(); // express app
+    return createHandler(instance);
+  }
